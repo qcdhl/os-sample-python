5546359c9e2a463c3d97320e326329f1ad513eb0
qcdhl <52079846+qcdhl@users.noreply.github.com>
2019-08-07 15:41:45 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from datetime import datetime
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    dateTimeObj = datetime.now()
+    timestampStr = dateTimeObj.strftime("%d.%m.%Y %H:%M:%S")
+    return "Hello World! " + timestampStr
 
 if __name__ == "__main__":
     application.run()
